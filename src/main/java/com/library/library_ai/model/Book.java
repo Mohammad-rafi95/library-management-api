bd2e623930b6efdd8c322fbf5b09069e09bf43e9
Shaik Mohammad Rafi <mdrafi.shaik34@gmail.com>
2026-08-01 18:03:00 +0530

    Fixed Book.java and pom.xml for deployment

--- src/main/java/com/library/library_ai/model/Book.java
+++ src/main/java/com/library/library_ai/model/Book.java
@@ -1,15 +1,14 @@
 package com.library.library_ai.model;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 @Entity
 @Table(name = "books")
-@Data
 public class Book {
 
     @Id
@@ -28,4 +27,38 @@ public class Book {
 
     private String genre;
     private String summary;
+
+    // Constructors
+    public Book() {}
+
+    public Book(String title, String author, String isbn, int publishedYear, String genre, String summary) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publishedYear = publishedYear;
+        this.genre = genre;
+        this.summary = summary;
+    }
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public int getPublishedYear() { return publishedYear; }
+    public void setPublishedYear(int publishedYear) { this.publishedYear = publishedYear; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public String getSummary() { return summary; }
+    public void setSummary(String summary) { this.summary = summary; }
 }
